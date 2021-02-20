2797dc2d8b29c6b915dd6f444f35b7c674cc08d3
mk3008 <mk3008.develop@gmail.com>
2021-02-21 00:55:31 +0900

    #5 SQLiteに対応
    SQLiteサンプル追加。

---
+++ samples/SQLiteSample/CorporationDatasourceMap.cs
@@ -0,0 +1,30 @@
+﻿using KeyMapSync;
+using System;
+using System.Collections.Generic;
+
+namespace SQLiteSample
+{
+    internal class CorporationDatasourceMap : IDatasourceMap
+    {
+        public string DestinationTableName => "client";
+
+        public string MappingName => "corporation";
+
+        public IEnumerable<string> DatasourceKeyColumns => new string[] { "corporation_id" };
+
+        public string DatasourceQuery => @"
+with datasource as (
+    select
+        corporation_name as client_name
+        , corporation_id
+    from
+        corporation
+    order by
+        corporation_id
+)";
+
+        public string DatasourceAliasName => "datasource";
+
+        public Func<object> ParameterGenerator => () => null;
+    }
+}
