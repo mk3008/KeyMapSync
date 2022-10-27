4aaff9b86a3e5d3aa52fb4ae6a4f31e8984aca80
mk3008 <mk3008.develop@gmail.com>
2022-10-27 20:09:06 +0900

    Delete unreferenced files

--- src/KeyMapSync/DBMS/ColumnPair.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KeyMapSync.DBMS;
-
-public class ColumnPair
-{
-    public string FromCloumn { get; set; } = string.Empty;
-
-    public string ToColumn { get; set; } = string.Empty;
-}
--- src/KeyMapSync/DBMS/SqlCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KeyMapSync.DBMS;
-
-public class SqlCommand
-{
-    public string CommandText { get; set; } = string.Empty;
-    public Dictionary<string, object> Parameters { get; set; } = new();
-}
-
